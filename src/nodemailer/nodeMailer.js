c89f7e2c0b70ca5dcf67cbd8bc2b2c9f49256193
Yadin Nou <nouyadin@gmail.com>
2026-09-04 07:49:00 +1000

    change port and secure:true

--- src/nodemailer/nodeMailer.js
+++ src/nodemailer/nodeMailer.js
@@ -6,7 +6,8 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   //port: 587,
   port: 465,
-  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  //secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
