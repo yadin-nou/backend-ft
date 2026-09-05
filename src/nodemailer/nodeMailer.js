af4ed6f8d6e48b860aba9c7e6ab5d82c87051b4b
Yadin Nou <nouyadin@gmail.com>
2026-09-05 11:30:05 +1000

    add secure:true

--- src/nodemailer/nodeMailer.js
+++ src/nodemailer/nodeMailer.js
@@ -4,14 +4,15 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   //host: "smtp.ethereal.email",
   host: "smtp.gmail.com",
-  port: 587,
-  //port: 465,
-  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
-  //secure: true,
+  //port: 587,
+  port: 465,
+  //secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  family: 4,
 });
 
 //Email processor
